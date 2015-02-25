f7b0d3c68b14fac90569040a5aae865d0a22498f
lpavesi <lpavesi@marchnetworks.com>
2015-02-25 12:51:43 +0100

    removed import of old xpcom

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@
 #/home/lore/Downloads/xulrunner-sdk/bin/xpidl -m typelib -w -v -I /home/lore/Downloads/xulrunner-sdk/idl/ -e xpcom/HeaderTool.xpt xpcom/HeaderTool.idl
 
 #cp xpcom/HeaderTool.xpt components/
-cp xpcom/HeaderTool.js components/
+#cp xpcom/HeaderTool.js components/
 rm ht.xpi
 #remove temporary files
 find ./ -name '*~'  -exec rm '{}' \; -print
-zip ht.xpi -r * -x xpcom\* build.sh example\* \*svn\*
+zip ht.xpi -r * -x xpcom\* build.sh example\* \*svn\* \*git\*
